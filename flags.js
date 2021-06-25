c3855c5f497eee4f21e8c0c0c456d7bc31aac37d
patilamisha1102 <86166357+patilamisha1102@users.noreply.github.com>
2021-06-26 00:40:37 +0530

    Add files via upload

---
+++ flags.js
@@ -0,0 +1,75 @@
+function brazilFunc()
+{
+    var c = " This is the flag of BRAZIL";
+    document.getElementById("brazil").innerHTML = c;
+}
+
+function outBrazil()
+{
+    var c=""
+    document.getElementById("brazil").innerHTML = c;
+}
+
+function indiaFunc()
+{
+    var c = " This is the flag of INDIA";
+    document.getElementById("india").innerHTML = c;
+}
+
+function outIndia()
+{
+    var c=""
+    document.getElementById("india").innerHTML = c;
+}
+
+function argFunc()
+{
+    var c = " This is the flag of ARGENTINA";
+    document.getElementById("arg").innerHTML = c;
+}
+
+function outArg()
+{
+    var c=""
+    document.getElementById("arg").innerHTML = c;
+}
+
+function franceFunc()
+{
+    var c = " This is the flag of FRANCE";
+    document.getElementById("france").innerHTML = c;
+}
+
+function outFrance()
+{
+    var c=""
+    document.getElementById("france").innerHTML = c;
+}
+
+function chinaFunc()
+{
+    var c = " This is the flag of CHINA";
+    document.getElementById("china").innerHTML = c;
+}
+
+function outChina()
+{
+    var c=""
+    document.getElementById("china").innerHTML = c;
+}
+
+function spainFunc()
+{
+    var c = " This is the flag of SPAIN";
+    document.getElementById("spain").innerHTML = c;
+}
+
+function outSpain()
+{
+    var c=""
+    document.getElementById("spain").innerHTML = c;
+}
+
+
+
+
